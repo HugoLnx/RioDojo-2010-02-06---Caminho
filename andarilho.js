8599fb6513bf80c8977fd57a0e084f11788b12a7
HugoLnx <hugolnx@gmail.com>
2010-06-03 15:44:47 -0700

    refatoracao 3: usando a nova funcao do mapa para expressivisar o codigo

--- andarilho.js
+++ andarilho.js
@@ -2,13 +2,12 @@ Andarilho = function(mapa,ponto_inicial){
 	this.mapa = mapa;
     this.localizacao = ponto_inicial;
 
-    this.da_um_passo = function (x){
-		matriz = this.mapa.matriz;
-		menor = [0,1];
-		if (matriz[menor[0]][menor[1]] > matriz[1][1])
-			menor = [1,1];
-		if (matriz[menor[0]][menor[1]] > matriz[1][0])
-			menor = [1,0];
-		this.localizacao = menor;
+    this.da_um_passo = function (){
+		coor_menor = [0,1];
+		if (mapa.valor_da_coordenada(coor_menor) > mapa.valor_da_coordenada([1,1]))
+			coor_menor = [1,1];
+		if (mapa.valor_da_coordenada(coor_menor) > mapa.valor_da_coordenada([1,0]))
+			coor_menor = [1,0];
+		this.localizacao = coor_menor;
 	}
 }
