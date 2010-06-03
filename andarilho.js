4c7f2689f1acadc7ded799c7335e3d4c5ba9d136
HugoLnx <hugolnx@gmail.com>
2010-06-03 12:12:39 -0700

    fazendo refatorando os testes, para eles falharem

--- andarilho.js
+++ andarilho.js
@@ -1,2 +1,3 @@
 Andarilho = function(){
+
 }
--- andarilho.test.js
+++ andarilho.test.js
@@ -1,4 +1,62 @@
 $(function(){
-module "Andarilho"
+module("Andarilho");
+	test("passo na diagonal começando na primeira posição", function() {
+		matriz = [[2, 4],
+				 [4, 1]];
+		
+		mapa = new Mapa(matriz);
+		coordenada_atual = [0,0];
+		andarilho = new Andarilho(mapa,coordenada_atual);
+		andarilho.da_um_passo;
 
+		same(andarilho.localizacao, [1,1]);
+	});
+
+	test("passo na lateral começando na primeira posição", function() {
+		matriz = [[2, 1],
+				 [4, 4]];
+
+		mapa = new Mapa(matriz);
+		coordenada_atual = [0,0];
+		andarilho = new Andarilho(mapa,coordenada_atual);
+		andarilho.da_um_passo;
+
+		same(andarilho.localizacao, [0,1]);
+	});
+
+	test("passo pra baixo começando na primeira posição", function() {
+		matriz = [[2, 4],
+				 [1, 4]];
+
+		mapa = new Mapa(matriz);
+		coordenada_atual = [0,0];
+		andarilho = new Andarilho(mapa,coordenada_atual);
+		andarilho.da_um_passo;
+
+		same(andarilho.localizacao, [1,0]);
+	});
+
+	test("passo pra baixo com valor diferente de 1", function() {
+		matriz = [[2, 5],
+				 [3, 4]];
+
+		mapa = new Mapa(matriz);
+		coordenada_atual = [0,0];
+		andarilho = new Andarilho(mapa,coordenada_atual);
+		andarilho.da_um_passo;
+
+		same(andarilho.localizacao, [1,0]);
+	});
+
+	test("passo pra direita com valor diferente de um", function() {
+		matriz = [[3, 2],
+				 [6, 4]];
+
+		mapa = new Mapa(matriz);
+		coordenada_atual = [0,0];
+		andarilho = new Andarilho(mapa,coordenada_atual);
+		andarilho.da_um_passo;
+
+		same(andarilho.localizacao, [0,1]);
+	});
 });
