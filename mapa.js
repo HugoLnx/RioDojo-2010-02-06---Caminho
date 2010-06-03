2794966c743a79b5c9d7cf835a807837c29a1be4
HugoLnx <hugolnx@gmail.com>
2010-06-03 15:39:13 -0700

    funcao de retornar valor apartir de uma coordenada esta funcionando e com os testes passando

--- mapa.js
+++ mapa.js
@@ -1,5 +1,10 @@
-Mapa = function (matriz){
+Mapa = function(matriz){
 	this.matriz = matriz;
 	
+	this.valor_da_coordenada = function(coordenada){
+		x = coordenada[0];
+		y = coordenada[1];
+		return this.matriz[x][y];
+	}
 	
 }
--- mapa.test.js
+++ mapa.test.js
@@ -1,7 +1,7 @@
 $(function(){
 module("Mapa");
 	test("retorna valor de uma coordenada", function() {
-		matriz = [[1,2]
+		matriz = [[1,2],
 				  [3,4]];
 		mapa = new Mapa(matriz);
 		same(mapa.valor_da_coordenada([0,0]),1);
